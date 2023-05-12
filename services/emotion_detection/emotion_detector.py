165b58f8503add45cd895127ef14e62084c18a28
Maryam Alrubaye <maryamalrubaye@dizaynvip.com>
2023-05-12 20:29:43 +0300

    Added emotion detector using deepface library.

---
+++ services/emotion_detection/emotion_detector.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+
+from deepface import DeepFace
+from deepface.detectors import FaceDetector
+
+
+@dataclass
+class DeepFaceAttributes:
+    """A class to store the attributes required for deepface emotion recognition.
+    """
+
+    detector_backend: str = "mtcnn"
+
+
+class EmotionDetector(DeepFaceAttributes):
+    def __int__(self):
+        self.activate()
+
+    def activate(self) -> None:
+        """Initialize the class and start the face recognition service."""
+        try:
+            FaceDetector.build_model(self.detector_backend)
+
+        except Exception as exception:
+            print(exception)
+
+    def detect_emotion(self, image):
+        Emotion = DeepFace.analyze(img_path=image,
+                                actions=['emotion'], detector_backend=self.detector_backend,
+                                )
+        return Emotion[0]['dominant_emotion']
+
+
+
