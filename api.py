5264002882ed60abee1d34c08f0376ddcc70b7fd
Nader Elziny <naderelziny805@gmail.com>
2023-05-16 17:21:38 +0300

    Added CORSMiddleware to the server.

--- api.py
+++ api.py
@@ -1,11 +1,21 @@
+import json
 import threading
 
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Request
 from services.face_recognition.Prediction import Predictor
 from services.face_recognition.Training import Trainer
+from fastapi.middleware.cors import CORSMiddleware
+from utils import convert_base64_to_image
+
 
 app = FastAPI()
+origins = ['http://localhost:3000/', 'http://localhost:8000/']
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
 @app.get("/train")
